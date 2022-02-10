d5b5713c4add463cb40ad149f9c548143285f5be
nikolaos-mavromatis <Nikolaos.Mavromatis@nike.com>
2022-02-10 19:38:37 +0100

    added sketch

--- entities/BoltWheelie/sketch.js
+++ entities/BoltWheelie/sketch.js
@@ -1,32 +1,33 @@
 var scrollPos = 0;
-
 function setup() {
     createCanvas(400, 400);
-    noStroke();
 
-    enemy = new BoltWheelie(width / 4, 0.9 * height, 20, 200);
+    enemy = new BoltWheelie(width / 4, 0.9 * height, 100, 200);
 }
 
 function draw() {
     background(120);
 
-    push();
-    translate(scrollPos);
-    enemy.draw();
-    pop();
+    if (!enemy.isDead) {
+        enemy.draw();
+        enemy.checkDead(mouseX, mouseY);
 
-    stroke(0)
-    strokeWeight(3);
-    line(width / 2 - scrollPos, 0, width / 2 - scrollPos, height);
-    noStroke();
-}
-
-function keyPressed() {
-    if (keyCode == 37) {
-        scrollPos += 5;
-    }
-    if (keyCode == 39) {
-        scrollPos -= 5;
+        hitEnemy = enemy.checkContact(mouseX, mouseY);
     }
 
+    //testing
+    //reference point
+    stroke("lightgreen");
+    strokeWeight(5);
+    point(enemy.currentX, enemy.y);
+    strokeWeight(1);
+
+    //interaction area
+    stroke("lightgreen");
+    strokeWeight(5);
+    point(enemy.currentX, enemy.y - enemy.size + enemy.r);
+    strokeWeight(1);
+    stroke(255);
+    noFill();
+    ellipse(enemy.currentX, enemy.y - enemy.size + enemy.r, 2 * enemy.r);
 }
