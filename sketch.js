9bcff8f6d5e454127b03fb7767e2d0aa7340dc8e
nikolaos-mavromatis <Nikolaos.Mavromatis@nike.com>
2022-02-18 10:24:33 +0100

    added comment

--- sketch.js
+++ sketch.js
@@ -4,6 +4,18 @@ Coursework 2.2 - Game Project Submission
 
 Week 20
 
+TODO Comment:
+    Apart from the sounds that are quite straightforward, I tried extending the basic requirements 
+by adding movement wherever possible. That proved to be quite challenging when it comes to collision detection and there are still small bugs when it comes to interaction with moving objects.
+    The easiest of all were the moving platforms where I followed the same principle as the enemies 
+and their range functionality. Quite easy was 
+    The most challenging however was the rotating collectable. Working with vectors, push/pop and 
+translate helped me understand better all the interactions and also how to create better animations.
+    I also tried implementing killing enemies by landing on top of them. Some are easier to kill than 
+others which was not intended and still doesn't make any sense.
+    Finally, what I wish I had done from the beginning was to create a separate p5 project for each 
+object and test its functionality there using dummy object or the cursor coordinates.
+
 */
 var LIVES = 3;
 
